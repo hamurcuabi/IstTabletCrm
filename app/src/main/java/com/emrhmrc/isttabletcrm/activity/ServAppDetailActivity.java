9a1726cf464ff5159d31648bba7ec7544d35a939
emirogs <emrehamurcu_1995@hotmail.com>
2018-12-31 11:19:46 +0300

    Generic Recyclerview Adapter Created and test is succesfully ended...

---
+++ app/src/main/java/com/emrhmrc/isttabletcrm/activity/ServAppDetailActivity.java
@@ -0,0 +1,15 @@
+package com.emrhmrc.isttabletcrm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.emrhmrc.isttabletcrm.R;
+
+public class ServAppDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serv_app_detail);
+    }
+}
