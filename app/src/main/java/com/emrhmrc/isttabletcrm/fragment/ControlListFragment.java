33ed70f594a331b260e32b39847c67de5d02f3dc
emirogs <emrehamurcu_1995@hotmail.com>
2019-01-02 16:55:26 +0300

    işemirler fragment  done

---
+++ app/src/main/java/com/emrhmrc/isttabletcrm/fragment/ControlListFragment.java
@@ -0,0 +1,33 @@
+package com.emrhmrc.isttabletcrm.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.emrhmrc.isttabletcrm.R;
+
+public class ControlListFragment extends DialogFragment {
+
+    public static ControlListFragment newInstance() {
+        Bundle args = new Bundle();
+        ControlListFragment fragment = new ControlListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.control_list_frag, container);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
