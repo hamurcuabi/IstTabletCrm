aa36d2b73e01fa48b71f1e901943a9c587013ca2
emirogs <emrehamurcu_1995@hotmail.com>
2019-01-03 22:08:21 +0300

    Yeni iş emri oluşturma ekranı done !

---
+++ app/src/main/java/com/emrhmrc/isttabletcrm/activity/CreateServAppActivity.java
@@ -0,0 +1,15 @@
+package com.emrhmrc.isttabletcrm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.emrhmrc.isttabletcrm.R;
+
+public class CreateServAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_serv_app);
+    }
+}
