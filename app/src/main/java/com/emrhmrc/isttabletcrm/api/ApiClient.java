64e35daee9928c1b0acd1f7c447f74a498258c46
emirogs <emrehamurcu_1995@hotmail.com>
2019-01-22 10:54:54 +0300

    yeni tasarımlar yapıldı fragmnetlar düzenlendi

--- app/src/main/java/com/emrhmrc/isttabletcrm/api/ApiClient.java
+++ app/src/main/java/com/emrhmrc/isttabletcrm/api/ApiClient.java
@@ -7,16 +7,18 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+    private static final int TIME_OUT = 15;
     private static final String TAG = "ApiClient";
+    private static final String USER_NAME = "Creatif";
+    private static final String PASSWORD = "Crm365@!";
     private static Retrofit retrofit = null;
     private static String Base_Url = "http://dpmdcturkliftmobiltest.innova.com.tr/api/";
 
     public static Retrofit getClient() {
         OkHttpClient okHttpClient = new OkHttpClient().newBuilder().addInterceptor
-                (new BasicAuthInterceptor("Creatif", "Crm365@!"))
-                .callTimeout(15, TimeUnit.SECONDS)
+                (new BasicAuthInterceptor(USER_NAME, PASSWORD))
+                .callTimeout(TIME_OUT, TimeUnit.SECONDS)
                 .build();
-
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
