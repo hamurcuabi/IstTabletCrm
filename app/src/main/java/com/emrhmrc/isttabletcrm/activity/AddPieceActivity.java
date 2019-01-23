315905181e9a7b508b8a483b11687b75e3cac2f4
emirogs <emrehamurcu_1995@hotmail.com>
2019-01-23 11:27:43 +0300

    Son tasarım adımları

---
+++ app/src/main/java/com/emrhmrc/isttabletcrm/activity/AddPieceActivity.java
@@ -0,0 +1,15 @@
+package com.emrhmrc.isttabletcrm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.emrhmrc.isttabletcrm.R;
+
+public class AddPieceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_piece);
+    }
+}
