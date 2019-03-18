3f59e7f3cc42d2b4977690695d5da7519b1de389
Emre Hmrc <emirogs1@gmail.com>
2019-03-18 16:11:13 +0300

    Update

--- app/src/main/java/com/emrhmrc/isttabletcrm/adapter/ServAppDescriptionViewHolder.java
+++ app/src/main/java/com/emrhmrc/isttabletcrm/adapter/ServAppDescriptionViewHolder.java
@@ -13,16 +13,19 @@ import com.emrhmrc.isttabletcrm.util.StringUtil;
 public class ServAppDescriptionViewHolder extends BaseViewHolder<ServAppGetByIdNotes,
         OnItemClickListener<ServAppGetByIdNotes>> {
 
-    private TextView txt_descp;
+    private TextView txt_descp, txt_owner, txt_statu;
 
     public ServAppDescriptionViewHolder(View itemView) {
         super(itemView);
         txt_descp = itemView.findViewById(R.id.txt_descp);
+        txt_owner = itemView.findViewById(R.id.txt_owner);
+        txt_statu = itemView.findViewById(R.id.txt_statu);
     }
 
     @Override
     public void onBind(final ServAppGetByIdNotes item, @Nullable final OnItemClickListener<ServAppGetByIdNotes> listener) {
         txt_descp.setText(StringUtil.nullToString(item.getSubject()));
+
 
     }
 
